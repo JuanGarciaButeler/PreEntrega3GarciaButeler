e8cf1b8b62a650287c5669d2f1e2666ffaedcc04
JuanGarciaButeler <129992039+JuanGarciaButeler@users.noreply.github.com>
2023-07-13 20:29:45 -0300

    Cambios

--- codigo.js
+++ codigo.js
@@ -1,5 +1,6 @@
-let usuario = prompt("ingrese su nombre de usuario") 
-let usuarioValidado = "Juan garcia"
+let usuario = prompt("ingrese su nombre de usuario")
+let usuarioValidado = "Juan Garcia"
+let codigoPromocional = [1234, 7654, 098, 34, 5]
 
 while (usuario != usuarioValidado){
     alert( "No se encontro el nombre de usuario")
@@ -7,5 +8,27 @@ while (usuario != usuarioValidado){
 }
 
 if (usuario = usuarioValidado){
-    alert("Hola " + usuarioValidado)    
+    alert("Hola " + usuarioValidado)
+}
+
+function validarCodigoPromocional ( codigoDeUsuario,codigoPromocional){
+    let flag = false
+    for (let index = 0;  index < codigoPromocional.length; index++) {
+        console.log(codigoDeUsuario)
+        if (codigoDeUsuario == codigoPromocional[index]){
+            flag = true
+        }else{
+            flag = false
+        }
+    }
+    return flag
+}
+
+let codigoDeUsuario = prompt("Ingrese codigo de usuario promocional")
+let validacion = validarCodigoPromocional (codigoDeUsuario, codigoPromocional)
+if (validacion == false){
+    alert("El codigo promocional no es valido")
+}
+if(validacion == true){
+    alert("El codigo promocional es valido") 
 }
